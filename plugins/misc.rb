8445fff4d12733175630668f562e29d0acae66a0
patrick brisbin <pbrisbin@gmail.com>
2011-10-30 18:39:40 -0400

    fix to funny "you" plugin

--- plugins/misc.rb
+++ plugins/misc.rb
@@ -2,9 +2,9 @@
 # basil, you're a bread compartment
 # => no, YOU are a bread compartment
 #
-Basil::Plugin.respond_to(/^(you are|you're)(.*)$/) {
+Basil::Plugin.respond_to(/^you(.*)$/) {
 
-  replies "no, YOU are#{@match_data[2]}!"
+  replies "no, YOU#{@match_data[1]}!"
 
 }
 
