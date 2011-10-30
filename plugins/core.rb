77bc64e90dd237a02b0717443a27cb6c47efc1da
patrick brisbin <pbrisbin@gmail.com>
2011-10-30 15:31:20 -0400

    add help, make reload work correctly

--- plugins/core.rb
+++ plugins/core.rb
@@ -8,17 +8,43 @@ Basil::Plugin.respond_to(/^test$/) {
 }.description = 'tests that the bot is working'
 
 #
+# Lists out regex/description for everythign basil will respond to / is
+# watching for in the chat
+#
+Basil::Plugin.respond_to(/^help$/) {
+
+  out = []
+
+  Basil::Plugin.responders.each do |p|
+    next unless p.description
+    out << "#{p.regex.inspect} => #{p.description}."
+  end
+
+  Basil::Plugin.watchers.each do |p|
+    next unless p.description
+    out << "#{p.regex.inspect} => #{p.description}."
+  end
+
+  says out.join("\n") unless out.empty?
+
+}
+
+#
 # Convenient for adjusting/adding plugins without restarting basil
 #
 Basil::Plugin.respond_to(/^reload$/) {
 
-  Basil::Plugin.class_eval do
-    @@responders = nil
-    @@watchers   = nil
-  end
+  a = Basil::Plugin.responders.length + Basil::Plugin.watchers.length
+
+  Basil::Plugin.responders.delete_if { true }
+  Basil::Plugin.watchers.delete_if { true }
+
+  b = Basil::Plugin.responders.length + Basil::Plugin.watchers.length
 
   Basil::Plugin.load!
 
-  replies 'done.'
+  c = Basil::Plugin.responders.length + Basil::Plugin.watchers.length
+
+  replies "#{a - b} plugins removed, #{c - b} plugins (re)loaded."
 
 }.description = 'reloads all files in the plugins directory'
