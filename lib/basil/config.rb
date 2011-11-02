bc12294517f3c5a20b4a606e936da5d578603428
patrick brisbin <pbrisbin@gmail.com>
2011-11-02 19:03:17 -0400

    remove some comments, add shell plugin

--- lib/basil/config.rb
+++ lib/basil/config.rb
@@ -4,18 +4,15 @@ module Basil
     class << self
 ################################################################################
 
-      # nick used to identify messages to me
       def me
         @@me ||= 'basil'
       end
 
-      # instance of server to run
       def server
         #@@server ||= Server::Cli.new
         @@server ||= Server::SkypeBot.new
       end
 
-      # directory holding plugin files
       def plugins_directory
         @@plugins_directory ||= './plugins'
       end
