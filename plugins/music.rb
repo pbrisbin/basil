8b9a30f595453aec5d282eab20a4935035d316d2
patrick brisbin <pbrisbin@gmail.com>
2011-11-02 14:37:32 -0400

    add a real whitelist for mpc commands

--- plugins/music.rb
+++ plugins/music.rb
@@ -1,16 +1,52 @@
-Basil::Plugin.respond_to(/^mpc.*/) {
+#
+# Supported actions:
+#
+#   mpc
+#   mpc current
+#   mpc play
+#   mpc next
+#   mpc prev
+#   mpc pause
+#   mpc toggle
+#   mpc playlist
+#   mpc lsplaylists
+#   mpc load <file>
+#   mpc stats
+#   mpc version
+#
+Basil::Plugin.respond_to(/^mpc load (.*)$/) {
 
-  require_authorization do
-    out = `#{@match_data[0]} 2>&1`
+  file = @match_data[1].strip rescue nil
+
+  if file && file != '' && File.exists?(file)
+    out = `mpc load #{file}`
     msg = '-- listen at http://pbrisbin.com:8000/mpd.mp3 --'
 
-    if $? == 0
-      says [out, msg].join("\n")
-    else
-      says out
-    end
+    says [out, msg].join("\n")
+  else
+    raise 'playlist file not found or not given'
   end
-  
+
+}
+
+Basil::Plugin.respond_to(/^mpc( (.*))?$/) {
+
+  arg = @match_data[2].strip rescue nil
+
+  valid_args = %w{ current play next prev pause toggle playlist
+                   lsplaylists stats version }
+
+  if !arg || arg == '' || valid_args.include?(arg)
+    out = `#{@match_data[0]}`
+    msg = '-- listen at http://pbrisbin.com:8000/mpd.mp3 --'
+
+    says [out, msg].join("\n")
+  else
+    # to match mpc help order
+    valid_args.insert(8, 'load <file>')
+    says "usage: mpc [#{valid_args.join('|')}]"
+  end
+
 }
 
 #
