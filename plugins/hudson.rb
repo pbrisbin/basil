61b7497b37213c964939001e3dac86bc0dfc3dfc
patrick brisbin <pbrisbin@gmail.com>
2011-11-02 22:06:09 -0400

    refactor hudson a bit

--- plugins/hudson.rb
+++ plugins/hudson.rb
@@ -17,69 +17,69 @@ module Basil
     end
   end
 
-  class HudsonJob
-    attr_reader :name, :url, :builds
+  module Hudson
+    class Job
+      attr_reader :name, :url, :builds
 
-    def initialize(json)
-      @name   = json['name']
-      @url    = json['url']
-      @stable = json['color'] =~ /^blue/
-      @builds = json['builds'].map {|b| HudsonBuild.new(b) } rescue []
-      @long_description = json['healthReport'].map { |d| d['description'] }.join("\n") rescue ''
-    end
+      def initialize(json)
+        @name   = json['name']
+        @url    = json['url']
+        @stable = json['color'] =~ /^blue/
 
-    def stable?
-      @stable
-    end
+        # present in per-job json
+        @builds = json['builds'].map {|b| HudsonBuild.new(b) } rescue []
+        @health = json['healthReport'].map { |d| d['description'] } rescue []
+      end
 
-    def short_description
-      if stable?
-        "#{@name} is stable"
-      else
-        "#{@name} is FAILING => #{@url}"
+      def stable?
+        @stable
+      end
+
+      def short_description
+        if stable?
+          "#{@name} is stable"
+        else
+          "#{@name} is FAILING => #{@url}"
+        end
+      end
+
+      def long_description
+        if stable?
+          short_description
+        else
+          ([short_description] + @health).join("\n")
+        end
       end
     end
 
-    def long_description
-      if stable?
-        short_description
-      else
-        [short_description, @long_description].join("\n")
+    class Build
+      attr_reader :number, :url
+
+      def initialize(json)
+        @number = json['number']
+        @url    = json['url']
       end
-    end
-  end
-
-  # TODO:
-  class HudsonBuild
-    attr_reader :number, :url
-
-    def initialize(json)
-    end
-
-    def passed?
     end
   end
 end
 
 Basil::Plugin.respond_to('hudson') {
 
-  out = []
-
   json = get_hudson_api('/')
-  json['jobs'].each do |job|
-    hudson_job = Basil::HudsonJob.new(job)
-    out << hudson_job.short_description
+  out  = json['jobs'].map do |job|
+    hudson_job = Basil::Hudson::Job.new(job)
+    hudson_job.short_description
   end
 
   says out.join("\n") unless out.empty?
 
 }.description = 'gives current hudson status'
 
-Basil::Plugin.respond_to(/^hudson (.*)/) {
+Basil::Plugin.respond_to(/^hudson (.+)/) {
 
-  job = @match_data[1].strip rescue ''
+  job  = @match_data[1].strip
   json = get_hudson_api("/job/#{job}/")
-  hudson_job = Basil::HudsonJob.new(json)
+  hudson_job = Basil::Hudson::Job.new(json)
 
   says hudson_job.long_description
 
