410c36fb72dd2f407b58656a5330ac8389349c99
patrick brisbin <pbrisbin@gmail.com>
2011-11-06 14:57:30 -0500

    rewrite hudson plugin (again)

--- plugins/hudson.rb
+++ plugins/hudson.rb
@@ -1,78 +1,113 @@
 module Basil
-  class Plugin
-    def get_hudson_api(path)
-      # Note: path must have a trailing slash
-      get_json(Config.hudson_host, path + 'api/json',
-               Config.hudson_port,
-               Config.hudson_user,
-               Config.hudson_password)
+  class HudsonApi
+    include Utils
+
+    # Note: path must include the trailing slash
+    def initialize(path)
+      @path = path
+      @json = nil
     end
-  end
 
-  module Hudson
-    class Job
-      attr_reader :name, :url, :builds
+    def method_missing(method, *args)
+      key = method.to_s
 
-      def initialize(json)
-        @name   = json['name']
-        @url    = json['url']
-        @stable = json['color'] =~ /^blue/
-
-        # present in per-job json
-        @builds = json['builds'].map { |b| HudsonBuild.new(b) } rescue []
-        @health = json['healthReport'].map { |d| d['description'] } rescue []
-      end
-
-      def stable?
-        @stable
-      end
-
-      def short_description
-        if stable?
-          "#{@name} is stable"
-        else
-          "#{@name} is FAILING => #{@url}"
-        end
-      end
-
-      def long_description
-        if stable?
-          short_description
-        else
-          ([short_description] + @health).join("\n")
-        end
+      if json.has_key?(key)
+        json[key]
+      else
+        super
       end
     end
 
-    class Build
-      attr_reader :number, :url
+    private
 
-      def initialize(json)
-        @number = json['number']
-        @url    = json['url']
+    def json
+      unless @json
+        @json = get_json(Basil::Config.hudson_host, @path + 'api/json',
+                         Basil::Config.hudson_port,
+                         Basil::Config.hudson_user,
+                         Basil::Config.hudson_password)
       end
+
+      @json
     end
   end
 end
 
-Basil::Plugin.respond_to('hudson') {
+Basil::Plugin.respond_to(/^hudson( (stable|failing))?$/) {
 
-  json = get_hudson_api('/')
-  out  = json['jobs'].map do |job|
-    hudson_job = Basil::Hudson::Job.new(job)
-    hudson_job.short_description
+  begin
+    status_line = lambda do |job|
+      " * #{job['name']} #{job['color'] =~ /blue/ ? "is stable." : "is FAILING. See #{job['url']} for details."}"
+    end
+
+    status = Basil::HudsonApi.new('/')
+
+    says_multiline do |out|
+      case (@match_data[2].strip rescue nil)
+      when 'stable'
+        out << "Current stable jobs:"
+        status.jobs.select { |job| job['color'] =~ /blue/ }.each { |job| out << status_line.call(job) }
+      when 'failing'
+        out << "Current failing jobs:"
+        status.jobs.reject { |job| job['color'] =~ /blue/ }.each { |job| out << status_line.call(job) }
+      else
+        out << "Current jobs:"
+        status.jobs.each { |job| out << status_line.call(job) }
+      end
+    end
+  rescue
+    says "There was an issue talking to hudson."
   end
 
-  says out.join("\n") unless out.empty?
-
-}.description = 'gives current hudson status'
+}.description = 'interacts with hudson'
 
 Basil::Plugin.respond_to(/^hudson (.+)/) {
 
-  job  = @match_data[1].strip
-  json = get_hudson_api("/job/#{job}/")
-  hudson_job = Basil::Hudson::Job.new(json)
+  begin
+    job = Basil::HudsonApi.new("/job/#{@match_data[1].strip}/")
 
-  says hudson_job.long_description
+    says_multiline("#{job.displayName} is #{job.color =~ /blue/ ? "stable" : "FAILING"}") do |out|
+      job.healthReport.each do |line|
+        out << line['description']
+      end
 
-}.description = 'gives current hudson build status'
+      out << "See #{job.url} for details."
+    end
+  rescue
+    says "Can't find info on #{@match_data[1]}"
+  end
+
+}.description = 'retrieves info on a specific hudson job'
+
+Basil::Plugin.respond_to(/who broke (.+)/) {
+
+  begin
+    job = Basil::HudsonApi.new("/job/#{@match_data[1].strip}/")
+
+    builds = job.builds.map { |b| b['number'].to_i }
+    last_stable = job.lastStableBuild['number'].to_i rescue nil
+
+    unless last_stable
+      return says "#{job.displayName} has been broken too long to know that."
+    end
+
+    if builds.first == last_stable
+      return says "#{job.displayName} is not broken."
+    end
+      
+    breaker = builds.select { |b| b > last_stable }.min
+
+    build = Basil::HudsonApi.new("/job/#{job.name}/#{breaker}/")
+
+    says_multiline("#{job.displayName} first broke in ##{breaker}:") do |out|
+      build.changeSet['items'].each do |item|
+        out << " * #{item['msg']} (#{item['user']} - r#{item['revision']})"
+      end
+
+      out << "See #{build.url} for details."
+    end
+  rescue
+    says "Can't find info on #{@match_data[1]}"
+  end
+
+}.description = 'tells you what commits lead to the first broken build'
