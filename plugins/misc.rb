cfb1d58356168b4d411e638a54e3b1860a8a6c9d
Jim Menard <jim@jimmenard.com>
2011-11-15 12:25:53 -0500

    added description to spark plugin

--- plugins/misc.rb
+++ plugins/misc.rb
@@ -31,4 +31,4 @@ Basil::Plugin.respond_to(/spark (.*)/) {
   values = @match_data[1].split(/[, ]+/).map { |x| x.to_f }
   min, range, scale = values.min, values.max - values.min, SPARK_TICKS.length - 1
   replies values.map { |x| SPARK_TICKS[(((x - min) / range) * scale).round] }.join
-}
+}.description "turns comma-separated list of numbers (int,float) into sparklines"
