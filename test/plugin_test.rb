cf8b9aa93d91f57812f917b5de8121405b747054
patrick brisbin <pbrisbin@gmail.com>
2011-11-16 01:31:13 -0500

    update dispatch tests for module method

--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -24,26 +24,26 @@ class TestPlugins < Test::Unit::TestCase
 
   def test_respond_to
     Plugin.respond_to('test') { true }
-    assert dispatch(Message.new(Config.me, nil, nil, 'test')), "Message should trigger"
-    assert !dispatch(Message.new(nil, nil, nil, 'test')), "Message not to me shouldn't trigger"
-    assert !dispatch(Message.new(Config.me, nil, nil, 'foo')), "Other Message shouldn't trigger"
+    assert Basil.dispatch(Message.new(Config.me, nil, nil, 'test')), "Message should trigger"
+    assert !Basil.dispatch(Message.new(nil, nil, nil, 'test')), "Message not to me shouldn't trigger"
+    assert !Basil.dispatch(Message.new(Config.me, nil, nil, 'foo')), "Other Message shouldn't trigger"
 
     Plugin.respond_to(/test (A|B)/) { true }
-    assert dispatch(Message.new(Config.me, nil, nil, 'test A')), "Message A should trigger"
-    assert dispatch(Message.new(Config.me, nil, nil, 'test B')), "Message B should trigger"
-    assert !dispatch(Message.new(nil, nil, nil, 'test A')), "Message not to me shouldn't trigger"
-    assert !dispatch(Message.new(Config.me, nil, nil, 'foo')), "Other Message shouldn't trigger"
+    assert Basil.dispatch(Message.new(Config.me, nil, nil, 'test A')), "Message A should trigger"
+    assert Basil.dispatch(Message.new(Config.me, nil, nil, 'test B')), "Message B should trigger"
+    assert !Basil.dispatch(Message.new(nil, nil, nil, 'test A')), "Message not to me shouldn't trigger"
+    assert !Basil.dispatch(Message.new(Config.me, nil, nil, 'foo')), "Other Message shouldn't trigger"
   end
 
   def test_watch_for
     Plugin.watch_for('test') { true }
-    assert dispatch(Message.new(nil, nil, nil, 'test')), "Message should trigger"
-    assert !dispatch(Message.new(nil, nil, nil, 'foo')), "Other Message shouldn't trigger"
+    assert Basil.dispatch(Message.new(nil, nil, nil, 'test')), "Message should trigger"
+    assert !Basil.dispatch(Message.new(nil, nil, nil, 'foo')), "Other Message shouldn't trigger"
 
     Plugin.watch_for(/test (A|B)/) { true }
-    assert dispatch(Message.new(nil, nil, nil, 'test A')), "Message A should trigger"
-    assert dispatch(Message.new(nil, nil, nil, 'test B')), "Message B should trigger"
-    assert !dispatch(Message.new(nil, nil, nil, 'foo')), "Other Message shouldn't trigger"
+    assert Basil.dispatch(Message.new(nil, nil, nil, 'test A')), "Message A should trigger"
+    assert Basil.dispatch(Message.new(nil, nil, nil, 'test B')), "Message B should trigger"
+    assert !Basil.dispatch(Message.new(nil, nil, nil, 'foo')), "Other Message shouldn't trigger"
   end
 
   def test_instance_variables
@@ -58,7 +58,7 @@ class TestPlugins < Test::Unit::TestCase
       true
     }
 
-    assert dispatch(msg)
+    assert Basil.dispatch(msg)
 
     msg = Message.new(nil, nil, nil, 'test text')
 
@@ -70,7 +70,7 @@ class TestPlugins < Test::Unit::TestCase
       true
     }
 
-    assert dispatch(msg)
+    assert Basil.dispatch(msg)
   end
 
   def test_dispatch
@@ -78,16 +78,16 @@ class TestPlugins < Test::Unit::TestCase
     Plugin.watch_for('test')  { :watcher   }
 
     msg = Message.new(Config.me, nil, nil, 'test')
-    assert_equal :responder, dispatch(msg), "Responder should respond"
+    assert_equal :responder, Basil.dispatch(msg), "Responder should respond"
 
     msg = Message.new(nil, nil, nil, 'test')
-    assert_equal :watcher, dispatch(msg), "Watcher should respond"
+    assert_equal :watcher, Basil.dispatch(msg), "Watcher should respond"
 
     msg = Message.new(Config.me, nil, nil, 'foo')
-    assert_nil dispatch(msg), "No one should respond"
+    assert_nil Basil.dispatch(msg), "No one should respond"
 
     msg = Message.new(nil, nil, nil, 'foo')
-    assert_nil dispatch(msg), "No one should respond"
+    assert_nil Basil.dispatch(msg), "No one should respond"
   end
 
   def test_dispatch_order
@@ -96,15 +96,15 @@ class TestPlugins < Test::Unit::TestCase
     Plugin.watch_for(/test/)   { :p3 }
 
     msg = Message.new(nil, nil, nil, 'tester')
-    assert_equal :p1, dispatch(msg), "First plugin should handle it"
+    assert_equal :p1, Basil.dispatch(msg), "First plugin should handle it"
 
     msg = Message.new(nil, nil, nil, 'teste')
-    assert_equal :p2, dispatch(msg), "Second plugin should handle it"
+    assert_equal :p2, Basil.dispatch(msg), "Second plugin should handle it"
 
     msg = Message.new(nil, nil, nil, 'test')
-    assert_equal :p3, dispatch(msg), "Third plugin should handle it"
+    assert_equal :p3, Basil.dispatch(msg), "Third plugin should handle it"
 
     msg = Message.new(nil, nil, nil, 'tes')
-    assert_nil dispatch(msg), "No one should handle it"
+    assert_nil Basil.dispatch(msg), "No one should handle it"
   end
 end
