094c3eae63999ce803f9b128791739509f704f44
patrick brisbin <pbrisbin@gmail.com>
2011-11-16 01:42:27 -0500

    update give plugin for module dispatch method

--- plugins/extra.rb
+++ plugins/extra.rb
@@ -6,8 +6,8 @@ Basil::Plugin.respond_to(/^(echo|say) (.*)/) {
 
 Basil::Plugin.respond_to(/^give (\w+) (.*)/) {
 
-  cmd = Basil::Message.new(Basil::Config.me, @match_data[1], @match_data[1], @match_data[2].strip)
-  dispatch(cmd)
+  msg = Basil::Message.new(Basil::Config.me, @match_data[1], @match_data[1], @match_data[2].strip)
+  Basil.dispatch(msg)
 
 }.description = 'executes a plugin replying to someone else'
 
