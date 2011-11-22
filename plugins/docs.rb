3bbdc10209394db0352755849dec06af0e6134d1
patrick brisbin <pbrisbin@gmail.com>
2011-11-22 10:44:17 -0500

    update docs link

--- plugins/docs.rb
+++ plugins/docs.rb
@@ -1,5 +1,5 @@
 Basil::Plugin.respond_to('docs') {
 
-  says "http://pbrisbin.com/static/rdocs/basil/"
+  says "http://pbrisbin.com/static/docs/ruby/basil/"
 
 }.description = 'links to bot documentation'
