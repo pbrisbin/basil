e3c1e270e8fbbbffaae49f0e0b44c31a3940e109
patrick brisbin <pbrisbin@gmail.com>
2011-12-07 18:36:07 -0500

    add persistent storage through PStore

---
+++ lib/basil/storage.rb
@@ -0,0 +1,26 @@
+module Basil
+  # Basil's persistent storage is implmented through simple, unchecked
+  # use of a PStore file. Plugins should take care not to step on other
+  # plugins' data.
+  #
+  # The Config key pstore_file defines the location of the pstore file,
+  # it defaults to /tmp/basil.pstore
+  module Storage
+    def self.with_storage
+      require "pstore"
+
+      @@pstore ||= ::PStore.new(pstore_file)
+      @@pstore.transaction do
+        result = yield @@pstore
+      end
+
+      result
+    end
+
+    private
+
+    def self.pstore_file
+      @@pstore_file ||= Config.pstore_file rescue '/tmp/basil.pstore'
+    end
+  end
+end
