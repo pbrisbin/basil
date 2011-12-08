3fdcd00b5e6c5c52555fccd41dd54d391eedd21d
patrick brisbin <pbrisbin@gmail.com>
2011-12-08 15:55:10 -0500

    change how give plugin works

--- plugins/give.rb
+++ plugins/give.rb
@@ -1,6 +1,12 @@
 Basil::Plugin.respond_to(/^give (\w+) (.*)/) {
 
-  msg = Basil::Message.new(Basil::Config.me, @match_data[1], @match_data[1], @match_data[2].strip)
-  Basil.dispatch(msg)
+  # form a message to basil without the give
+  msg = Basil::Message.new(Basil::Config.me, @msg.from, @msg.from_name, @match_data[2].strip)
+
+  # set the ivar as basil's reply
+  @msg = Basil.dispatch(msg)
+
+  # so we can forward it on
+  @msg ? forwards_to(@match_data[1]) : nil
 
 }.description = 'executes a plugin replying to someone else'
