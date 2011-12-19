38100d6987b3f795fb9013c7da0f0cc071f72ff2
patrick brisbin <pbrisbin@gmail.com>
2011-12-19 16:15:17 -0500

    only nag once about messages

--- plugins/messages.rb
+++ plugins/messages.rb
@@ -6,21 +6,29 @@ Basil::Plugin.respond_to(/^tell ([^:]*): (.+)/) {
 
   Basil::Storage.with_storage do |store|
     store[:tell_messages] ||= []
+    store[:tell_notified] ||= {}
+
     store[:tell_messages] << { :time => Time.now, :to => to, :from => from, :message => msg }
+    store[:tell_notified][@msg.from] = false
   end
 
   replies "consider it noted."
 
-}.description = "Leave a message for something"
+}.description = "Leave a message for someone"
 
 Basil::Plugin.respond_to(/^(do i have any |any )?messages\??$/i) {
 
   Basil::Storage.with_storage do |store|
+    store[:tell_messages] ||= []
+    store[:tell_notified] ||= {}
+
     msgs = store[:tell_messages].select { |msg| @msg.from_name =~ /#{msg[:to]}/i }
 
     if msgs.empty?
       replies "sorry, I have no messages for you."
     else
+      store[:tell_notified][@msg.from] = false
+
       replies("your messages:") do |out|
         msgs.each do |msg|
           out << "#{msg[:time].strftime("On %D, at %r")}, #{msg[:from]} wrote:"
@@ -38,11 +46,21 @@ Basil::Plugin.respond_to(/^(do i have any |any )?messages\??$/i) {
 
 Basil::Plugin.watch_for(/.*/) {
 
-  msgs = Basil::Storage.with_storage do |store|
-    store[:tell_messages].select { |msg| @msg.from_name =~ /#{msg[:to]}/i }
+  msgs = notified = nil
+
+  Basil::Storage.with_storage do |store|
+    store[:tell_messages] ||= []
+    store[:tell_notified] ||= {}
+
+    msgs = store[:tell_messages].select { |msg| @msg.from_name =~ /#{msg[:to]}/i }
+    notified = store[:tell_notified][@msg.from]
   end
 
-  if !msgs.nil? && !msgs.empty?
+  if !msgs.nil? && !msgs.empty? && !notified
+    Basil::Storage.with_storage do |store|
+      store[:tell_notified][@msg.from] = true
+    end
+
     len = msgs.length
 
     # plularize correctly
