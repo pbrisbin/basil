a6dd1a98498ead3987b7e6a049cd1d8a57c30098
patrick brisbin <pbrisbin@gmail.com>
2011-12-19 16:26:12 -0500

    add seen plugin

---
+++ plugins/seen.rb
@@ -0,0 +1,25 @@
+Basil::Plugin.log do
+  Basil::Storage.with_storage do |store|
+    store[:last_seen] ||= {}
+    store[:last_seen][@msg.from] = { :name => @msg.from_name, :when => Time.now }
+  end
+end
+
+Basil::Plugin.respond_to(/^seen (.+?)\??$/) {
+
+  seen = nil
+
+  Basil::Storage.with_storage do |store|
+    store[:last_seen] ||= {}
+    store[:last_seen].each_value do |v|
+      $stderr.puts @match_data.inspect
+      if v[:name] =~ /#{@match_data[1].strip}/i
+        seen = v
+        break
+      end
+    end
+  end
+
+  seen ? replies("#{seen[:name]} was last seen on #{seen[:when].strftime("%D, at %r")}.") : nil
+
+}.description = "displays when the person was last seen in chat"
