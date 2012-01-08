bc5299b2ee1424e3f51ee716b476352b33101de4
patrick brisbin <pbrisbin@gmail.com>
2012-01-08 01:13:29 -0500

    this commit message is useless.

---
+++ plugins/commit.rb
@@ -0,0 +1,6 @@
+# https://github.com/github/hubot-scripts/blob/master/src/scripts/commitmessage.coffee
+Basil::Plugin.respond_to(/commit ?message/i) {
+
+  says get_http("http://whatthecommit.com/index.txt").body rescue nil
+
+}.description = "give a random commit message"
