3fb67e65b24d4043db68eb5686c97500537fec6c
patrick brisbin <pbrisbin@gmail.com>
2012-01-08 01:08:10 -0500

    chuck norris plugin ported.

---
+++ plugins/chuck_norris.rb
@@ -0,0 +1,7 @@
+Basil::Plugin.watch_for(/(chuck norris|jack bauer)/i) {
+
+  query = @match_data[0].downcase == 'jack bauer' ?  '?firstName=Jack&lastName=Bauer' : ''
+
+  says get_json("http://api.icndb.com/jokes/random#{query}")['value']['joke'] rescue nil
+
+}
