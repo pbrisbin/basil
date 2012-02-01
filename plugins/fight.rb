dd278350700ebf0b0a238a0cf27d1d388635fd28
patrick brisbin <pbrisbin@gmail.com>
2012-02-01 18:56:37 -0500

    add fight plugin

---
+++ plugins/fight.rb
@@ -0,0 +1,18 @@
+Basil::Plugin.respond_to(/^fight (\S+) (\S+)/) {
+
+  play = lambda do |a,b|
+    score_a = (1..9001).to_a.shuffle.first
+    score_b = (1..9001).to_a.shuffle.first
+
+    if score_a == score_b
+      play.call(a,b)
+    else
+      "#{a}: #{score_a}, #{b}: #{score_b}"
+    end
+  end
+
+  score = play.call(@match_data[1], @match_data[2])
+
+  says score
+
+}.description = 'plays out a fictional battle between two combatants'
