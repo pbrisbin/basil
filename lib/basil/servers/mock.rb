86aa4ab26a2465e1029cf8282c9727c93342d52c
patrick brisbin <pbrisbin@gmail.com>
2012-03-23 23:52:44 -0400

    add realistic process to mock server

--- lib/basil/servers/mock.rb
+++ lib/basil/servers/mock.rb
@@ -5,6 +5,18 @@ module Basil
       def run
 
       end
+
+      def process(text, to = Config.me)
+        msg = Message.new(to, 'test-user', 'Test User', text, 'test')
+
+        ChatHistory.store_message(msg)
+
+        if reply = Basil.dispatch(msg)
+          return reply
+        end
+
+        nil
+      end
     end
   end
 end
