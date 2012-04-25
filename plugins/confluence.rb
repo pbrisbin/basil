ab67ad1a4dcebc31cbd268386f7b8914ac971f87
patrick brisbin <pbrisbin@gmail.com>
2012-04-24 22:03:06 -0400

    add confluence/wiki plugin

---
+++ plugins/confluence.rb
@@ -0,0 +1,16 @@
+Basil.respond_to(/^(confluence|wiki) (.+)/) {
+
+  begin
+    xml = get_xml(symbolize_keys(Basil::Config.confluence).merge(
+      :path => "/rest/prototype/1/search?query=#{escape(@match_data[2])}"))
+
+    result = xml['results']['result']
+    result = result.first if result.is_a?(Array)
+
+    says result['link'].first['href']
+
+  rescue
+    says 'no results found.'
+  end
+
+}.description = 'searches confluence'
