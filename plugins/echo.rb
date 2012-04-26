5621261993bf26df0a8353b174e9c428d1525b8e
patrick brisbin <pbrisbin@gmail.com>
2012-04-26 17:37:52 -0400

    remove pipe/sub from plugins

--- plugins/echo.rb
+++ plugins/echo.rb
@@ -1,26 +1,5 @@
-# says will be a little cute with "I am"
-Basil.respond_to(/^say (.*)/) {
+Basil.respond_to(/^(echo|say) (.*)/) {
 
-  says @match_data[1].strip.sub(/^basil\s+is\b/i, 'I am')
-
-}.description = "says what it's told"
-
-# echo is more programmer-friendly and has command substitution
-Basil.respond_to(/^echo (.*)/) {
-
-  msg = @match_data[1].strip
-
-  # neat part is it'll do it recursively.
-  while m = /(.*)\$\((.*)\)(.*)/.match(msg)
-    pref, sub, suf = m.captures
-
-    if reply = Basil.dispatch(Basil::Message.new(Basil::Config.me, @msg.from, @msg.from_name, sub))
-      msg = "#{pref}#{reply.text}#{suf}"
-    else
-      msg = "I'm sorry, `#{sub}' is invalid."
-    end
-  end
-
-  says msg
+  says @match_data[2].strip.sub(/^basil\s+is\b/i, 'I am')
 
 }.description = "says what it's told"
