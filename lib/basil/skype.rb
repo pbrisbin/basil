bc3f2cb0c82839822598d6fa4f016598ff7eab35
patrick brisbin <pbrisbin@gmail.com>
2012-06-15 12:09:07 -0400

    restructure skype start and comment oddities

--- lib/basil/skype.rb
+++ lib/basil/skype.rb
@@ -3,22 +3,40 @@ require 'rype'
 module Basil
   class Skype < Server
     def start
-      # for some reason, using the nested block approach seems to make
-      # things quicker and more consistent
+      # Note: there are a number of oddities in how the dbus connection
+      # behaves. Also, if you break this code by violating the below,
+      # you won't get an exception, basil just stops listening.
+      #
+      # Therefore, if you touch this method, keep in mind the following:
+      #
+      # 1. You must use nested blocks when you need to access multiple
+      #    parts of a skype object.
+      #
+      # 2. The order of your nesting seems to matter. Though I've yet to
+      #    determine how or why.
+      #
+      # 3. Sometimes, it's required you use the block argument
+      #    immediately.
+      #
+      # 4. Sometimes, it's required you don't pass the block argument to
+      #    any external methods.
+      #
       Rype.on(:chatmessage_received) do |chatmessage|
-        chatmessage.chat do |chat|
-          chat.members do |members|
-            is_private = members.length == 2
-
-            chatmessage.from do |from|
-              chatmessage.from_name do |from_name|
-                chatmessage.body do |body|
-                  to, text = parse_body(body)
+        chatmessage.from do |from|
+          chatmessage.from_name do |from_name|
+            chatmessage.body do |body|
+              chatmessage.chat do |chat|
+                chat.members do |members|
+                  is_private = members.length == 2
+                  to, text   = parse_body(body)
 
                   to  = Config.me if !to && is_private
                   msg = Message.new(to, from, from_name, text, chat.chatname)
 
-                  send_message(chat, reply) if reply = dispatch_message(msg)
+                  if reply = dispatch_message(msg)
+                    prefix = reply.to ? "#{reply.to.split(' ').first}, " : ''
+                    chat.send_message(prefix + reply.text)
+                  end
                 end
               end
             end
@@ -34,7 +52,7 @@ module Basil
         chats.each do |chat|
           chat.topic do |topic|
             if [topic, chat.chatname].include?(msg.chat)
-              send_message(chat, msg)
+              chat.send_message(msg.text)
             end
           end
         end
@@ -42,11 +60,6 @@ module Basil
     end
 
     private
-
-    def send_message(chat, msg)
-      prefix = msg.to ? "#{msg.to.split(' ').first}, " : ''
-      chat.send_message(prefix + msg.text)
-    end
 
     def parse_body(body)
       case body
