5b4521382e7200197b2e28d4c2e9438ad9a9f849
patrick brisbin <pbrisbin@gmail.com>
2012-07-06 14:00:34 -0400

    add spec on config class

---
+++ spec/config_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+module Basil
+  describe Config do
+    before do
+      Config.invalidate
+
+      @conf = { 'key_foo' => 'foo',
+                'key_bar' => 'bar',
+                'server_type' => :cli }
+
+      File.stub(:read).and_return(@conf.to_yaml)
+    end
+
+    it "has accessors from the config file" do
+      Config.key_foo.should == 'foo'
+    end
+
+    it "uses server_type to instantiate the right server" do
+      Config.server.should be_a(Cli)
+    end
+
+    it "can be hidden" do
+      Config.hide do
+        Config.key_foo.should be_nil
+      end
+    end
+  end
+end
