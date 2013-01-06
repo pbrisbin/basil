80c84a3289170c2587c8f00581f1d3f3228827dc
patrick brisbin <pbrisbin@gmail.com>
2013-01-05 19:54:35 -0500

    simplify plugin class

--- lib/basil/plugin.rb
+++ lib/basil/plugin.rb
@@ -8,29 +8,22 @@ module Basil
 
     private_class_method :new
 
-    def self.create(type, regex, &block)
-      p = new(type, regex)
-      p.define_singleton_method(:execute, &block)
-      p.register!
-    end
-    private_class_method :create
-
     # Create an instance of Plugin which will look for regex only in
     # messages that are to basil.
     def self.respond_to(regex, &block)
-      create(:responder, regex, &block)
+      new(:responder, regex, &block)
     end
 
     # Create an instance of Plugin which will look for regex in any
     # messages sent in the chat.
     def self.watch_for(regex, &block)
-      create(:watcher, regex, &block)
+      new(:watcher, regex, &block)
     end
 
     # Create an instance of Plugin which will look for regex in the
     # subject of any emails basil receives.
     def self.check_email(regex, &block)
-      create(:email_checker, regex, &block)
+      new(:email_checker, regex, &block)
     end
 
     def self.responders
@@ -62,13 +55,17 @@ module Basil
     attr_reader :type, :regex
     attr_accessor :description
 
-    def initialize(type, regex)
-      if regex.is_a? String
-        regex = Regexp.new("^#{regex}$")
-      end
+    def initialize(type, regex, &block)
+      @type  = type
+      @regex = regex.is_a?(String) ? Regexp.new("^#{regex}$") : regex
 
-      @type, @regex = type, regex
-      @description  = nil
+      define_singleton_method(:execute, &block)
+
+      case type
+      when :responder    ; Plugin.responders     << self
+      when :watcher      ; Plugin.watchers       << self
+      when :email_checker; Plugin.email_checkers << self
+      end
     end
 
     # if the message's text matches our regex, set the proper instance
@@ -91,18 +88,10 @@ module Basil
       nil
     end
 
-    def register!
-      case @type
-      when :responder    ; Plugin.responders     << self
-      when :watcher      ; Plugin.watchers       << self
-      when :email_checker; Plugin.email_checkers << self
-      end; self
-    end
-
     # avoiding #to_s to preserve #inspect, see
     # http://bugs.ruby-lang.org/issues/4453.
     def pretty
-      "#{@type}: #{@regex.inspect}"
+      "#{type}: #{regex.inspect}"
     end
   end
 end
