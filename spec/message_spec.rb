b91ac067be2356893c1134ec91046261b735bdbd
patrick brisbin <pbrisbin@gmail.com>
2013-01-11 18:31:45 -0500

    rewrite Message spec

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,81 +1,105 @@
 require 'spec_helper'
-require 'timecop'
 
 module Basil
-  describe 'Message as dispatchable' do
-    subject { Message.new(:from => 'x') }
+  describe Message, '#initialize' do
+    subject { described_class }
 
-    it_behaves_like "a Dispatchable"
-  end
-
-  describe 'Message construction' do
-    it "should raise when from is not given" do
-      lambda { Message.new(:to => 'you') }.should raise_error(ArgumentError)
+    it "should raise on invalid arguments" do
+      lambda { subject.new(:to => 'you') }.should raise_error(ArgumentError)
     end
 
-    it "can be constructed with only from" do
-      msg = Message.new(:from => 'me')
+    it "works with only from specified" do
+      msg = subject.new(:from => 'x')
+
+      msg.from.should      == 'x'
+      msg.from_name.should == 'x'
+      msg.text.should      == ''
+
+      msg.to.should be_nil
+      msg.chat.should be_nil
+    end
+  end
+
+  describe Message, 'from_message' do
+    subject { described_class }
+
+    it "constructs a new message from the first" do
+      msg = subject.from_message(described_class.new(:from => 'me'), :to => 'you')
 
       msg.from.should == 'me'
-      msg.from_name.should == 'me'
-      msg.text.should == ''
-    end
-
-    it "can be constructed from another message" do
-      msg  = Message.new(:from => 'me')
-      msg2 = Message.from_message(msg, :to => 'you')
-
-      msg2.from.should == 'me'
-      msg2.to.should == 'you'
+      msg.to.should   == 'you'
     end
   end
 
-  describe 'Message attributes' do
-    it "has a text attribute" do
-      msg = Message.new(:from => 'x', :text => 'text')
+  describe Message, 'instance' do
+    subject { described_class.new(:from => 'x') }
 
-      msg.text.should == 'text'
-    end
+    it_behaves_like "a Dispatchable"
 
     it "has accessible to and chat attributes" do
-      msg = Message.new(:from => 'x', :to => 'to', :chat => 'chat')
+      subject.to = 'other to'
+      subject.chat = 'other chat'
 
-      msg.to.should == 'to'
-      msg.chat.should == 'chat'
-
-      msg.to = 'other to'
-      msg.chat = 'other chat'
-
-      msg.to.should == 'other to'
-      msg.chat.should == 'other chat'
+      subject.to.should == 'other to'
+      subject.chat.should == 'other chat'
     end
 
-    it "sets a time attribute when created" do
-      Timecop.freeze do
-        msg = Message.new(:from => 'me')
-        msg.time.should == Time.now
+    it "sets a time attribute" do
+      subject.time.should_not be_nil
+    end
+
+    it "provides to_me? case insensitively" do
+      Config.stub(:me).and_return('someone')
+
+      subject.to_me?.should be_false
+
+      %w( someone SomeOne SOMEONE ).each do |me|
+        subject.to = me
+        subject.to_me?.should be_true
       end
     end
   end
 
-  describe 'Message#to_me?' do
-    it "is a case insensitive match" do
-      Config.stub(:me).and_return('me')
-
-      msg = Message.new(:from => 'x', :to => 'me')
-      msg.to_me?.should be_true
-
-      msg.to = 'Me'
-      msg.to_me?.should be_true
-
-      msg.to = 'you'
-      msg.to_me?.should be_false
+  describe Message, 'with a server' do
+    subject do
+      described_class.new(
+        :to        => 'to',
+        :from      => 'from',
+        :from_name => 'from name',
+        :text      => 'text',
+        :chat      => 'chat'
+      )
     end
-  end
 
-  describe 'Message sending' do
-    it "can say things"
-    it "can reply to itself"
-    it "can forward itself"
+    let(:server) do
+      Struct.new(:sent_messages) do
+        def send_message(msg)
+          self.sent_messages << "#{msg.to}, #{msg.text}"
+        end
+      end.new([])
+    end
+
+    before do
+      subject.server = server
+    end
+
+    it "can say things" do
+      subject.say "some text"
+      subject.say "some other text"
+
+      server.sent_messages.should == [', some text', ', some other text']
+    end
+
+    it "can reply to itself" do
+      subject.reply "some text"
+
+      server.sent_messages.should == ['from name, some text']
+    end
+
+    it "can forward itself" do
+      subject.forward('new to')
+
+      server.sent_messages.should == ['new to, text']
+    end
   end
 end
