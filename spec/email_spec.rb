1a2f059fd6e2e2842783be868af84c19aa69971f
patrick brisbin <pbrisbin@gmail.com>
2013-01-11 19:28:43 -0500

    improve Email spec

--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -1,37 +1,45 @@
 require 'spec_helper'
 
 module Basil
-  describe Email, 'check' do
+  describe Email do
+    subject { described_class }
+
+    let(:imap) { double }
+
+    let(:attrs) do
+      double(:attr => {'RFC822' => 'message body'})
+    end
+
     before do
-      Thread.stub(:new).and_yield
-      Email.stub(:poll_email?).and_return(false)
+      Net::IMAP.stub(:new).and_return(imap)
+
+      Thread.stub(:new).and_yield # so we don't fork
+      subject.stub(:poll_email?).and_return(false) # so we don't loop
+      #subject.stub(:loop).and_yield # so we don't loop
 
 
-      @imap = double("imap", :store => nil)
-      Email.stub(:with_imap).and_yield(@imap)
     end
 
-    it "should search imap" do
-      @imap.should_receive(:search).and_return([])
+    it "should check for mail" do
+      # before
+      imap.should_receive(:login)
+      imap.should_receive(:select)
 
-      Email.check
-    end
+      # the search
+      imap.should_receive(:search).and_return(['message_id'])
+      imap.should_receive(:fetch).with('message_id', 'RFC822').and_return([attrs])
 
-    it "should parse and dispatch mails" do
-      server = double("server")
-      Config.stub(:server).and_return(server)
-
-      attrs = double('attrs', :attr => {'RFC822' => 'message body'})
-
-      @imap.should_receive(:search).and_return(['message_id'])
-      @imap.should_receive(:fetch).with('message_id', 'RFC822').and_return([attrs])
-
+      # the handling
       mail = mock
-      mail.should_receive(:dispatch).with(server)
+      mail.should_receive(:dispatch).with(Config.server)
 
       Email::Mail.should_receive(:parse).with('message body').and_return(mail)
 
-      Email.check
+      # after
+      imap.should_receive(:logout)
+      imap.should_receive(:disconnect)
+
+      subject.check
     end
   end
 end
