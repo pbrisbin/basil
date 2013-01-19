3fb0e62c0fc21edccc1954656c89dbe7b2695c4b
patrick brisbin <pbrisbin@gmail.com>
2013-01-18 22:24:36 -0500

    small daemon refactor, add spec

--- lib/basil/daemon.rb
+++ lib/basil/daemon.rb
@@ -1,53 +1,42 @@
 module Basil
   module Daemon
     class << self
+
       def start(foreground = Config.foreground?)
         if foreground
           Config.server.start
         else
-          start_in_background
+          pid = fork_process
+          puts "forked. pid: #{pid}"
         end
       end
 
       def stop
-        if pid = read_pid
-          system("kill #{pid}")
-        end
+        pid = File.read(Config.pid_file).strip rescue nil
+        pid && system("kill #{pid}")
       end
 
       private
 
-      def start_in_background
-        pid = fork do
+      def fork_process
+        fork do
           Loggers.output = Config.log_file
 
           logger.info "=== Started: #{Process.pid} ==="
 
-          write_pid
+          File.open(Config.pid_file, 'w') do |fh|
+            fh.puts "#{Process.pid}"
+          end
+
           close_io
 
           Config.server.start
         end
-
-        puts "forked. pid: #{pid}"
-      end
-
-      def write_pid
-        File.open(Config.pid_file, 'w') do |fh|
-          fh.puts "#{Process.pid}"
-        end
-      end
-
-      def read_pid
-        File.read(Config.pid_file).strip
-      rescue => ex
-        logger.debug ex; nil
       end
 
       def close_io
         [STDIN, STDOUT].each do |io|
-          begin
-            io.reopen("/dev/null")
+          begin io.reopen("/dev/null")
           rescue Exception
           end
         end
---
+++ spec/basil/daemon_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+module Basil
+  describe Daemon do
+    subject { described_class }
+
+    let(:server) { double("server").as_null_object }
+
+    before { Config.stub(:server).and_return(server) }
+
+    context "start" do
+      context "when in forground" do
+        it "starts the server" do
+          server.should_receive(:start)
+          subject.start(true)
+        end
+      end
+
+      context "when in background" do
+        before do
+          Config.stub(:foreground?).and_return(false)
+
+          subject.stub(:puts)
+          subject.stub(:fork).and_yield
+
+          File.stub(:open).and_yield(double.as_null_object)
+
+          [STDIN, STDOUT, STDERR].each do |io|
+            io.stub(:reopen)
+            io.stub(:sync)
+          end
+        end
+
+        it "sets up logging to the log-file" do
+          Loggers.should_receive(:output=).with(Config.log_file)
+
+          subject.start
+        end
+
+        it "writes a pid file" do
+          Process.stub(:pid).and_return(123)
+
+          fh = double("file handle")
+          fh.should_receive(:puts).with("123")
+
+          File.should_receive(:open).with(Config.pid_file, 'w').and_yield(fh)
+
+          subject.start
+        end
+
+        it "closes file descriptors" do
+          STDIN.should_receive(:reopen).with("/dev/null")
+          STDOUT.should_receive(:reopen).with("/dev/null")
+          STDERR.should_receive(:reopen).with(STDOUT)
+
+          subject.start
+        end
+
+        it "starts the server" do
+          Config.server.should_receive(:start)
+
+          subject.start
+        end
+      end
+    end
+
+    context "stop" do
+      it "kills the pid found in the pid-file" do
+        File.should_receive(:read).with(Config.pid_file).and_return(" 123 ")
+        subject.should_receive(:system).with("kill 123")
+
+        subject.stop
+      end
+    end
+
+  end
+end
