0b336fd61786f89bd4d97f71e7d320262a3e6474
patrick brisbin <pbrisbin@gmail.com>
2013-01-21 13:42:14 -0500

    clean up airbrake plugin a bit

--- plugins/airbrake.rb
+++ plugins/airbrake.rb
@@ -1,54 +1,55 @@
-module Basil
-  class Airbrake
-    include Utils
+class Airbrake
+  HOST ||= [Basil::Config.airbrake['account'], 'airbrake', 'io'].join('.')
 
-    def self.pretty_print(group, msg)
-      most_recent_at = group["most-recent-notice-at"]["__content__"]
-      notices_count  = group["notices-count"]["__content__"]
-      error_id       = group["id"]["__content__"]
-      error_message  = group["error-message"]
-      error_class    = group["error-class"]
-
-      error_url = "https://#{Config.airbrake['account']}.airbrakeapp.com/errors/#{error_id}"
-
-      msg.say ""
-      msg.say " ##{error_id}(#{notices_count}) last seen:#{most_recent_at}"
-      msg.say " #{error_class}: #{error_message}"
-      msg.say " => #{error_url}"
+  class ErrorGroup
+    def initialize(group_xml)
+      @group = group_xml
     end
 
-    def initialize
-      @host = [Config.airbrake['account'], 'airbrake', 'io'].join('.')
+    def to_s
+      most_recent_at = @group['most-recent-notice-at']['__content__']
+      notices_count  = @group['notices-count']['__content__']
+      error_id       = @group['id']['__content__']
+      error_message  = @group['error-message']
+      error_class    = @group['error-class']
 
-      # we assume we only care about one environment and it's specified
-      # in the config file as project.
-      project = Config.airbrake['project'].to_s
-
-      if project and !project.empty?
-        @path = "/projects/#{project}/errors.xml?auth_token=#{Config.airbrake['token']}"
-      else
-        @path = "/errors.xml?auth_token=#{Config.airbrake['token']}"
-      end
+      <<-EOS
+        ---
+        ##{error_id}(#{notices_count}) last seen:#{most_recent_at}
+        #{error_class}: #{error_message}
+        => https://#{HOST}/errors/#{error_id}
+      EOS
     end
+  end
 
-    def method_missing(meth, *args)
-      return xml[meth.to_s] if xml
-    end
+  def initialize(plugin)
+    # we assume we only care about one environment and it's specified
+    # in the config file as project.
+    project = "#{Basil::Config.airbrake['project']}"
+    token   = "#{Basil::Config.airbrake['token']}"
 
-    def xml
-      @xml ||= get_xml('host' => @host, 'port' => 443, 'path' => @path)
+    path = if project != ''
+             "/projects/#{project}/errors.xml?auth_token=#{token}"
+           else
+             "/errors.xml?auth_token=#{token}"
+           end
+
+    @xml = plugin.get_xml('host' => HOST, 'port' => 443, 'path' => path)
+  end
+
+  def groups(limit = 5)
+    @xml['groups']['group'].take(limit).map do |g|
+      ErrorGroup.new(g)
     end
   end
 end
 
 Basil.respond_to(/^(show me )?airbrake( errors)?/i) {
 
-  xml = Basil::Airbrake.new
-
   @msg.reply "5 most recent airbrake errors:"
 
-  xml.groups["group"][0..5].each do |group|
-    Basil::Airbrake.pretty_print(group, @msg)
+  Airbrake.new(self).groups.each do |g|
+    @msg.say trim("#{g}")
   end
 
 }.description = "shows the five most recent airbrake errors in production."
