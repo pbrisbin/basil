5a36beec80b727297526cd649bffc29b1b7e99dd
patrick brisbin <pbrisbin@gmail.com>
2013-01-22 16:08:23 -0500

    fix isitdown plugin

--- plugins/isitdown.rb
+++ plugins/isitdown.rb
@@ -2,9 +2,6 @@ Basil.respond_to(/^is (\S+) down\??$/) {
 
   text = get_html("http://www.isup.me/#{@match_data[1]}").at('#container').children.to_s
 
-  # note, relying on assumptions about this sites html...
-  if text && text =~ /\n *\n(.*)\n/
-    @msg.say $1.gsub(/<\/?a.*?>/, '')
-  end
+  @msg.say text.strip.split("\n").first.strip.gsub(/<\/?a.*?>/, '')
 
 }.description = "see if a site is down for everyone or just you"
