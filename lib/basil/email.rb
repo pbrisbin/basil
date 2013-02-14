d769a052219833cb0aae80781e785791e9089a0c
patrick brisbin <pbrisbin@gmail.com>
2013-02-14 09:13:11 -0500

    update Email to use Timer/Worker

--- lib/basil/email.rb
+++ lib/basil/email.rb
@@ -8,56 +8,22 @@ module Basil
 
       attr_reader :thread
 
-      def check(once = false)
-        @thread = spawn_timer_thread(once)
+      def check
+        interval = Config.email['interval'] || 30
 
-        logger.info "Spawned email monitor #{thread}"
+        @thread = Timer.new(:sleep => interval) do
+          logger.info "Checking email"
+
+          Worker.new do
+            checker = Checker.new
+            checker.run
+          end
+
+          logger.info "Email check done"
+        end
       end
 
       private
-
-      def spawn_timer_thread(once)
-        Thread.new do
-          loop do
-            fork_worker
-
-            break if once
-
-            logger.debug "Sleeping thread for #{interval} seconds"
-
-            sleep(interval)
-          end
-        end
-      end
-
-      def fork_worker
-        pid = Process.fork do
-          worker = Worker.new
-          worker.run
-        end
-
-        logger.debug "Spawned worker with PID #{pid} to check mail"
-
-        babysit(pid)
-
-      rescue => ex
-        logger.warn ex
-      end
-
-      # wait for our child, then kill them
-      def babysit(pid)
-        Timeout.timeout(30) do
-          Process.wait(pid)
-          logger.debug "Worker terminated. Status: #{$?.exitstatus}"
-        end
-      rescue Timeout::Error
-        logger.warn "Killing worker with PID #{pid} (timeout)"
-        logger.debug `kill -9 #{pid} 2>&1`
-      end
-
-      def interval
-        Config.email['interval'] || 30
-      end
 
       def logger
         @logger ||= Loggers['email']
--- spec/basil/email_spec.rb
+++ spec/basil/email_spec.rb
@@ -2,19 +2,37 @@ require 'spec_helper'
 
 module Basil
   describe Email do
-    it "should fork the worker in a thread and store the thread" do
-      thread = double("thread")
-      Thread.should_receive(:new).and_yield.and_return(thread)
+    let(:checker) do
+      double("Checker").tap do |checker|
+        Email::Checker.stub(:new).and_return(checker)
+      end
+    end
 
-      Process.should_receive(:fork).and_yield.and_return(123)
+    before do
+      Timer.stub(:new).and_yield
+      Worker.stub(:new).and_yield
+    end
 
-      worker = double("worker")
-      worker.should_receive(:run)
-      Email::Worker.stub(:new).and_return(worker)
+    it "should run the email checker" do
+      checker.should_receive(:run)
 
-      Process.should_receive(:wait).with(123)
+      Email.check
+    end
 
-      Email.check(true) # once
+    it "should pass interval to the timer" do
+      Config.stub(:email).and_return({'interval' => 5 })
+
+      Timer.should_receive(:new).with(:sleep => 5)
+
+      Email.check
+    end
+
+    it "should set thread" do
+      thread = double("Timer thread")
+
+      Timer.stub(:new).and_return(thread)
+
+      Email.check
 
       Email.thread.should == thread
     end
